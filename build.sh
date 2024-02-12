1d0eedd0141d9ad4fb6216168bfd060b1493f8dd
hello_world <haileformal@gmail.com>
2024-02-13 00:37:37 +0300

    move project build and make to single script

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
-cd build ; make
+# This script is used to run cmake and make for the project
+cmake -DGLFW_BUILD_DOCS=OFF -S . -B ./build
+
+# Run make to build the project
+cd build
+make
