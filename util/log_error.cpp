1d4089dc76edde5331c71c418b3955dda9828cea
hello_world <haileformal@gmail.com>
2024-02-14 01:29:02 +0300

    add a util to check and log gl errors

---
+++ util/log_error.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <GL/glew.h>
+
+
+void glClearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+bool glCheckError(const char* functionName, const char* file, int line) {
+    GLenum error = glGetError();
+    if (error != GL_NO_ERROR) {
+        std::cerr << "OpenGL error: " << error << " in function " << functionName
+                  << " at " << file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
+}
---
+++ util/log_error.h
@@ -0,0 +1,2 @@
+void glClearError();
+bool glCheckError(const char* functionName, const char* file, int line);
