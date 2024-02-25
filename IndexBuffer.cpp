8ecd531bcce83f81a549c735dc18aa1e04ad71ff
hello_world <haileformal@gmail.com>
2024-02-26 01:58:31 +0300

    stop deleting buffer storage on buffer abstraction objects' destruction

--- IndexBuffer.cpp
+++ IndexBuffer.cpp
@@ -1,5 +1,6 @@
 #include "Indexbuffer.h"
 #include <GL/glew.h>
+#include <iostream>
 
 
 IndexBuffer::IndexBuffer(
@@ -12,8 +13,17 @@ IndexBuffer::IndexBuffer(
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, length * sizeof(indices), indices, draw_type);
 }
 
-IndexBuffer::~IndexBuffer() {}
 
-void IndexBuffer::bind() const {}
+// TODO: The destructor should be the VAO destructor is called not when the object is destroyed
+IndexBuffer::~IndexBuffer() {
+    std::cout << "calling IndexBuffer destructor" << std::endl;
+    // glDeleteBuffers(1, &this->m_id);
+}
 
-void IndexBuffer::unbind() const{}
+void IndexBuffer::bind() const {
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->m_id);
+}
+
+void IndexBuffer::unbind() const{
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
--- VertexBuffer.cpp
+++ VertexBuffer.cpp
@@ -1,4 +1,3 @@
-#include <GL/glew.h>
 #include "VertexBuffer.h"
 
 
@@ -11,8 +10,39 @@ VertexBuffer::VertexBuffer(
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), draw_type);
 }
 
-VertexBuffer::~VertexBuffer(){}
 
-void VertexBuffer::bind() const {}
+VertexBuffer::~VertexBuffer(){
+    std::cout << "calling VertexBuffer destructor" << std::endl;
+    // glDeleteBuffers(1, &this->m_id);
+}
 
-void VertexBuffer::unbind() const{}
+
+void VertexBuffer::bind() const {
+    glBindBuffer(GL_ARRAY_BUFFER, this->m_id);
+}
+
+
+void VertexBuffer::unbind() const{
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+
+void VertexBuffer::addAttribute(VertexAttribute vertex_attribute){
+    this->m_vertex_attributes.push_back(vertex_attribute);
+}
+
+
+void VertexBuffer::setAttributePointers() const{
+    for (unsigned int i = 0; i < this->m_vertex_attributes.size(); i++){
+        VertexAttribute vertex_attribute = this->m_vertex_attributes[i];
+        glEnableVertexAttribArray(i);
+        glVertexAttribPointer(
+            i, 
+            vertex_attribute.m_vector_length, 
+            vertex_attribute.m_type, 
+            vertex_attribute.m_normalized, 
+            vertex_attribute.m_stride,
+            (void*)vertex_attribute.m_offset
+        );
+    }
+}
