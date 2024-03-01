b5b2dbb00291b8880b9cfc968ca65ede3a1dffea
hello_world <haileformal@gmail.com>
2024-03-01 15:16:13 +0300

    update time to use glfwGetTime

--- util/Time.cpp
+++ util/Time.cpp
@@ -1,24 +1,21 @@
-#include <chrono>
 #include "Time.h"
 
+Time::Time() {}
+Time::~Time() {}
 
-Time::Time(){}
-Time::~Time(){}
-
-
-std::chrono::time_point<std::chrono::high_resolution_clock> Time::m_last_frame_time = std::chrono::high_resolution_clock::now();
+float Time::m_last_frame_time = glfwGetTime();
 
 float Time::m_delta_time = 0.0f;
 
-
 // Update the current time and calculate delta time
-void Time::update() {
-    auto current_frame_time = std::chrono::high_resolution_clock::now();
-    m_delta_time = std::chrono::duration<float>(current_frame_time - m_last_frame_time).count();  //count gives us second
+void Time::update()
+{
+    float current_frame_time = glfwGetTime();
+    m_delta_time = current_frame_time - m_last_frame_time;
     m_last_frame_time = current_frame_time;
 }
 
-
-float Time::deltaTime() {
+float Time::deltaTime()
+{
     return m_delta_time;
 }
--- util/Time.h
+++ util/Time.h
@@ -1,8 +1,8 @@
-#include <chrono>
+#include <GLFW/glfw3.h>
 
 class Time {
 private:
-    static std::chrono::time_point<std::chrono::high_resolution_clock> m_last_frame_time;
+    static float m_last_frame_time;
     static float m_delta_time; // Delta time in seconds
 
 public:
