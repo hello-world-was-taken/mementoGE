511d5b173d7c1031beceb6dbdb2c7187c8ea1735
hello_world <haileformal@gmail.com>
2024-09-22 21:00:53 +0100

    add glew include to cmake

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
+# Check if the out directory exists, if not create it
+if [ ! -d ./out ]; then
+  mkdir -p out;
+fi
+
 # This script is used to run cmake and make for the project
 cmake -DGLFW_BUILD_DOCS=OFF -S . -B ./out
 
